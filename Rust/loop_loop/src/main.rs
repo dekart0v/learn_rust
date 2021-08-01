3d4efc74dc8c95fd9234c25c4a0329cc8b9bb469
dekart0v <xiuden00@vutbr.cz>
2021-08-02 00:34:15 +0200

    added loop_loop

--- Rust/loop_loop/src/main.rs
+++ Rust/loop_loop/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("loop_loop");
+
+    let mut i = 0;
+
+    loop {
+        i += 1;
+        println!("the value of N is {}", i);
+
+        if i == 5 {
+            continue; // back to the begining of the loop
+        }
+
+        if i >= 10 { 
+            break; // stop the loop
+        }
+    }
 }
